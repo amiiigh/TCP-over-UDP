03dadb0ab3b5c0ad5e43c0afb8ad5cb87a97a0a7
amiiigh <ghafari.ah@gmail.com>
2019-12-04 20:48:25 -0500

    lock added

---
+++ src/server.js
@@ -0,0 +1,29 @@
+var rudp = require('./index');
+var dgram = require('dgram');
+var fs = require('fs');
+var args = process.argv.slice(2);
+var serverPort = args[0]
+var serverSocket = dgram.createSocket('udp4');
+serverSocket.bind(serverPort);
+console.log('server is running on ', serverPort)
+_connections = {};
+timerIsRunning = false;
+var totalSize = 0
+serverSocket.on('message', function (message, rinfo) {
+	var addressKey = rinfo.address + rinfo.port;
+	var connection;
+	if (!_connections[addressKey]) {
+		connection = new rudp.Connection(new rudp.PacketSender(serverSocket, rinfo.address, rinfo.port));
+		_connections[addressKey] = connection;
+		connection.on('data', (data) => {
+			totalSize += data.length
+			console.log(totalSize)
+		})
+	} else {
+		connection = _connections[addressKey];
+	}
+	var packet = new rudp.Packet(message);
+	setImmediate(function () {
+		connection.receive(packet);
+	});
+});
